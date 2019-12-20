5b4236fc47809584a88e420f2e2cc09eb20f88fe
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-20 11:33:47 -0600

    Creates first test for Login endpoint

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -3,3 +3,29 @@ const request = require('supertest');
 const server = require('../api/server.js');
 
 const db = require('../database/dbConfig');
+
+describe('Authrouter', function() {
+    describe('environment', function() {
+        it('should set the environment to testing', function() {
+            expect(process.env.DB_ENV).toBe('testing');
+        })
+    })
+    // LOGIN POST TESTs
+    describe('Post /', function() {
+        beforeEach(async function() {
+            await db('users').truncate();
+        });
+        describe('add user', function() {
+            it('should return a 201 OK', function() {
+                return request(server)
+                    .post('/api/login')
+                    .send({ username: 'Zack', password: 'Testing' })
+                    .then(res => {
+                        expect(res.status).toBe(201);
+                    })
+            })
+        })
+    })
+})
+
+
