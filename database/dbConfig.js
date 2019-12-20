2d1701cc18a939e7bcec637b40e6160de2aa3ff7
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-20 10:51:41 -0600

    dbConfig file is configured

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
-
-module.exports = knex(knexConfig.development);
+const environment = process.env.DEB_ENV || 'development';
+module.exports = knex(knexConfig[environment]);
