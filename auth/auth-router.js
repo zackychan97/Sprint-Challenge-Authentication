e88407c6ab957ec28acedc2e3a942f9e71db89a4
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-20 11:12:27 -0600

    Creat register endpoint hashing password, create login endpoint, assign token once verification of pw compareSync

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -9,10 +9,44 @@ const userDb = require('../models/userDb');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 8);
+
+    user.password = hash;
+
+    userDb.add(user)
+      .then(stored => {
+        res.status(200).json(stored);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      })
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  userDb.getBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        //sign token
+        const token = signToken(user);
+
+        //send the token
+        res.status(200).json({
+          token,
+          message: `Welcome ${user.username}!`,
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({message: "There was an error logging in", error});
+      });
 });
 
 module.exports = router;
