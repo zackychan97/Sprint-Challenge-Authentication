7ec4cbf09e3bc4b7be3fe270e0e72f80d494ab3e
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-20 10:47:27 -0600

    authenticate middleware created

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,24 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken'); 
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+    const { authorization } = req.headers;
+
+    if (authorization) {
+      const secret = process.env.JWT_SECRET;
+
+      jwt.verify(authorization, secret, function(err, decodedToken) {
+        if (err) {
+          res.status(401).json({ you: 'shall not pass!' });
+        } else {
+          req.token = decodedToken;
+
+          next();
+        }
+      });
+    } else {
+      res.status(400).json({ message: 'Login and try again, please.' });
+    }
 };
